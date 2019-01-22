e4cf72fdd6c77c311e9ad8d88bb82d2f1c01189d
niyz <Simon.nilsson123@gmail.com>
2019-01-22 20:44:26 +0100

    commenting

--- Source Files/Game/Block.cpp
+++ Source Files/Game/Block.cpp
@@ -4,4 +4,6 @@
 Block::Block(std::uint16_t xPos, std::uint16_t yPos, std::uint16_t startValue, std::uint16_t colourR, std::uint16_t colourG, std::uint16_t colourB)
 {
 	//this is a comment
+
+	//nah bruh, THIS is a comment.
 }
