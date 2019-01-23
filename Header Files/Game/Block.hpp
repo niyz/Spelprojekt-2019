93823261ba6489c6003a658fe7c3607b4e6b1f76
SNILS <simon.nilsson123@gmail.com>
2019-01-23 11:01:42 +0100

    Created the Block class. Skipped some constructors.

--- Header Files/Game/Block.hpp
+++ Header Files/Game/Block.hpp
@@ -10,7 +10,47 @@ class Block
 		std::uint16_t startValue;
 		std::uint8_t colourR, colourG, colourB;
 	public:
-		Block(std::uint16_t xPos, std::uint16_t yPos, std::uint16_t startValue, std::uint16_t colourR, std::uint16_t colourG, std::uint16_t colourB);
+		Block(std::uint16_t xPos, std::uint16_t yPos, std::uint16_t startValue, std::uint8_t colourR, std::uint8_t colourG, std::uint8_t colourB);
+			//Skall fungera som default konstruktor med rimliga startvärden.
+		Block(const Block&);
+			//Copy-konstruktor..? Deep/Shallow?
+		Block &operator=(const Block&);
+			//Tilldelningsoperator
+		bool operator==(const Block&) const;
+			//Jämförelseoperator
+		bool operator!=(const Block&) const;
+			//Jämförelseoperator
+		
+		/*__________________Funktioner_______________________*/
+
+		std::uint16_t GetXPosition() const;
+		std::uint16_t GetYPosition() const;
+		std::uint16_t GetValue() const;
+	
+		std::uint8_t GetColourR() const;
+		std::uint8_t GetColourG() const;
+		std::uint8_t GetColourB() const;
+
+		void ColourAsArray(std::uint8_t *colour) const;
+			//Antag att denna pekare är till en array med 3 platser (RGB)
+
+		void SetXPosition(std::uint16_t);
+		void SetYPosition(std::uint16_t);
+		void SetValue(std::uint16_t);
+
+		void SetColourR(std::uint8_t);
+		void SetColourG(std::uint8_t);
+		void SetColourB(std::uint8_t);
+
+		void SetColour(std::uint8_t *colour);
+			//Antag att denna pekare är till en array med 3 platser (RGB)
+
+
+
+
+
+
+
 };
 
 
--- Source Files/Game/Block.cpp
+++ Source Files/Game/Block.cpp
@@ -1,11 +1,104 @@
 #include "..\..\Header Files\Game\Block.hpp"
 
 
-Block::Block(std::uint16_t xPos, std::uint16_t yPos, std::uint16_t startValue, std::uint16_t colourR, std::uint16_t colourG, std::uint16_t colourB)
+Block::Block(std::uint16_t xPos, std::uint16_t yPos, std::uint16_t startValue, std::uint8_t colourR, std::uint8_t colourG, std::uint8_t colourB)
 {
-	//this is a comment
+	this->xPos = xPos;
+	this->yPos = yPos;
+	this->startValue = startValue;
+	this->colourR = colourR;
+	this->colourG = colourG;
+	this->colourB = colourB;
+}
 
-	//nah bruh, THIS is a comment.
+Block::Block(const Block &)
+{
+	//??
+}
 
-	//Cool bruhv, git 100% ok yes.
+Block & Block::operator=(const Block &)
+{
+	// TODO: insert return statement here
+}
+
+bool Block::operator==(const Block &) const
+{
+	return false;
+}
+
+bool Block::operator!=(const Block &) const
+{
+	return false;
+}
+
+std::uint16_t Block::GetXPosition() const
+{
+	return this->xPos;
+}
+
+std::uint16_t Block::GetYPosition() const
+{
+	return this->yPos;;
+}
+
+std::uint16_t Block::GetValue() const
+{
+	return this->startValue;
+}
+
+std::uint8_t Block::GetColourR() const
+{
+	return this->colourR;
+}
+
+std::uint8_t Block::GetColourG() const
+{
+	return this->colourG;
+}
+
+std::uint8_t Block::GetColourB() const
+{
+	return this->colourB;
+}
+
+void Block::ColourAsArray(std::uint8_t * colour) const
+{
+	
+}
+
+void Block::SetXPosition(std::uint16_t xPosArg)
+{
+	this->xPos = xPosArg;
+}
+
+void Block::SetYPosition(std::uint16_t yPosArg)
+{
+	this->yPos = yPosArg;
+
+}
+
+void Block::SetValue(std::uint16_t startValueArg)
+{
+	this->startValue = startValueArg;
+}
+
+void Block::SetColourR(std::uint8_t colourRArg)
+{
+	this->colourR = colourRArg;
+}
+
+void Block::SetColourG(std::uint8_t colourGArg)
+{
+	this->colourG = colourGArg;
+}
+
+void Block::SetColourB(std::uint8_t colourBArg)
+{
+	this->colourB = colourBArg;
+
+}
+
+void Block::SetColour(std::uint8_t * colour)
+{
+	// ?
 }
